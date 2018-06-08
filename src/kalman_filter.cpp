987a311a6c8706883bb095fad27afa1616167bc1
Jumana MP <jumanamp91@gmail.com>
2018-06-08 20:19:24 +0100

    Implements Kalman Filter Methods

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,7 +3,7 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-// Please note that the Eigen library does not initialize 
+// Please note that the Eigen library does not initialize
 // VectorXd or MatrixXd objects with zeros upon creation.
 
 KalmanFilter::KalmanFilter() {}
@@ -22,9 +22,11 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 
 void KalmanFilter::Predict() {
   /**
-  TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd F_t = F_.transpose();
+  P_ = F_ * P_ * F_t + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,11 +34,50 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si; // Kalman Filter Gain
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
-  TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  // 1. Convert state values from cartesian to polar
+  float rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+  if (fabs(rho) < 0.0001) {
+    rho = 0.0001;
+  }
+  float phi = atan2(x_(1) + M_1_PI, x_(0));
+  float rho_dot = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
+
+
+  // 2. Apply Kalman Filter
+  VectorXd z_pred(3);
+  z_pred << rho, phi, rho_dot;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+
+
 }
